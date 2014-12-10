0bfde986b73ab26016b88b97eb6aec2ab3ad06a4
mbocher <maxence.bocher@gmail.com>
2014-12-10 17:51:28 +0100

    participate 2

--- front/participate.php
+++ front/participate.php
@@ -3,14 +3,17 @@
 session_start();
 include('lib/dbconnect.php');
 include('header.php');
-//var_dump($_SESSION['data']->id);
-//var_dump($_GET['event']);exit;
+
+?>
+<div id="content">
+    <div class="container">
+<?php
 if (isset($_GET['event'])) {
 //protection pour éviter doublon dans la base
-    $check = $bdd->prepare('SELECT COUNT(id) AS count FROM event_user WHERE client=' . $_SESSION['data']->id . ' AND event=' . $_GET['event']);
+    $check = $bdd->prepare('SELECT COUNT(id) AS count FROM event_user WHERE user=' . $_SESSION['data']->id . ' AND event=' . $_GET['event']);
     $check->execute();
     $checked = $check->fetch(PDO::FETCH_OBJ);
-//    var_dump($checked->count);exit;
+    //si on ne participe pas à cette VP
     if ($checked->count == 0) {
         $req = $bdd->prepare('INSERT INTO event_user VALUES(NULL, :status, :user, :event, :date )');
         $req->execute(array(
@@ -20,9 +23,11 @@ if (isset($_GET['event'])) {
             'date' => date('Y-m-d H:i:s')
         ));
         $data = $req->fetch(PDO::FETCH_OBJ);
+        echo '<h1>Félicitation vous participez à cette vente privée !</h1>';
     } else {
         echo '<h1>Vous participez déjà à cette vente privée !</h1>';
     }
+    echo '<a href="index.php">< retour</a>';
 } else {
     header('location:index.php');
 }
